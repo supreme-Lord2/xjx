9e35dddc39f83dcec7a2a125d507776c8eae0e52
SuprEmE2 <166421298+Vinpink2@users.noreply.github.com>
2026-04-02 15:31:47 +0300

    Update config.js

--- config.js
+++ config.js
@@ -71,7 +71,7 @@ module.exports = {
       adminOnly: '🛡️ This command is only for group admins!',
       groupOnly: '👥 This command can only be used in groups!',
       privateOnly: '💬 This command can only be used in private chat!',
-      botAdminNeeded: '🤖 Bot needs to be admin to execute this command!',
+      botAdminNeeded: '🚫 Bot needs to be admin to execute this command!',
       invalidCommand: '❓ Invalid command! Type .menu for help'
     },
     
