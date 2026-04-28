f3a0b3c7e41fc73be72a561a9b6cbc7f36c0efee
dot-666 <dotreplit@gmail.com>
2026-04-28 13:28:13 +0300

    Add files via upload

---
+++ config (1).js
@@ -0,0 +1,91 @@
+module.exports = {
+    ownerNumber: ['254798952773','254792021944'],
+    ownerName: ['Supreme', ''],
+    
+    botName: 'June-Ultra',
+    prefix: '.',
+    version: '2.8.1',
+    sessionName: 'session',
+    sessionID: process.env.SESSION_ID || '',
+    newsletterJid: '',
+    updateZipUrl: 'https://github.com/Vinpink2/June-X-Ultra/archive/refs/heads/main.zip',
+    
+    packname: '✮⃝𝐒ᵘᵖʳᵉᵐᵉ',
+    
+    selfMode: false,
+    autoRead: false,
+    autoTyping: false,
+    autoBio: false,
+    autoSticker: false,
+    autoReact: false,
+    autoReactMode: 'bot',
+    autoDownload: false,
+    autoRecording: false,
+    autoRecordType: false,
+    
+    defaultGroupSettings: {
+      antilink: false,
+      antilinkAction: 'delete',
+      antitag: false,
+      antitagAction: 'delete',
+      antiall: false,
+      antiviewonce: false,
+      antibot: false,
+      anticall: false,
+      antigroupmention: false,
+      antigroupmentionAction: 'delete',
+      antigroupstatus: false,
+      antigroupstatusAction: 'delete',
+      welcome: false,
+      welcomeMessage: ' 𝚆𝙴𝙻𝙲𝙾𝙼𝙴: @user 👋\n Member count: #memberCount\n 𝚃𝙸𝙼𝙴: time⏰\n\n\n*@user* Welcome to *@group*! 🎉\n*Group 𝙳𝙴𝚂𝙲𝚁𝙸𝙿𝚃𝙸𝙾𝙽*\ngroupDesc\n\n> *ᴘᴏᴡᴇʀᴇᴅ ʙʏ botName*',
+      welcomeNoPP: false,
+      goodbye: false,
+      goodbyeMessage: 'Goodbye @user 👋 We will never miss you!',
+      antiSpam: false,
+      antiSpamLimit: 5,
+      antiSpamWindow: 5,
+      antiSpamAction: 'delete',
+      antidelete: false,
+      nsfw: false,
+      detect: false,
+      chatbot: false,
+      autosticker: false,
+      antiimage: false,
+      antiimageAction: 'delete',
+      antisticker: false,
+      antistickerAction: 'delete',
+      antiaudio: false,
+      antiaudioAction: 'delete',
+      antibadword: false,
+      antibadwordAction: 'warn',
+      badwords: []
+    },
+    
+    apiKeys: {
+      openai: '',
+      deepai: '',
+      remove_bg: ''
+    },
+    
+    messages: {
+      wait: '⏳ Please wait...',
+      success: '✅ Success!',
+      error: '❌ Error occurred!',
+      ownerOnly: '👑 This command is only for bot owner!',
+      adminOnly: '🛡️ This command is only for group admins!',
+      groupOnly: '👥 This command can only be used in groups!',
+      privateOnly: '💬 This command can only be used in private chat!',
+      botAdminNeeded: '🚫 Bot needs to be admin to execute this command!',
+      invalidCommand: '❓ Invalid command! Type .menu for help'
+    },
+    
+    timezone: 'Africa/Nairobi',
+    
+    maxWarnings: 3,
+    
+    social: {
+      github: 'https://github.com/Vinpink2/June-Ultra',
+      instagram: 'https://instagram.com/activator_negative',
+      youtube: 'http://youtube.com/@suprem_e_lord'
+    }
+};
