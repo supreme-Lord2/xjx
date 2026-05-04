1c3876fd349ea513125a36a1687420cdf5c8524e
dot-666 <dotreplit@gmail.com>
2026-05-04 22:59:46 +0300

    Add files via upload

--- commands/tools/fetch.js
+++ commands/tools/fetch.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-const config = require('../../config');
+const config = require(require('path').join(global.__ROOT__, 'config'));
 
 module.exports = {
     name: 'fetch',
