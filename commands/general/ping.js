350621dca8f70be3b252d19ca87b0e6e1fa4fe73
dot-666 <dotreplit@gmail.com>
2026-06-13 11:04:00 +0300

    Update ping.js

--- commands/general/ping.js
+++ commands/general/ping.js
@@ -2,23 +2,6 @@
 const config = require('../../config');
 const { applyFont } = require('../../utils/fontConverter');
 
-function createFakeContact(message) {
-    return {
-        key: {
-            participants: "0@s.whatsapp.net",
-            remoteJid: "",
-            fromMe: false,
-            id: "JUNE-X"
-        },
-        message: {
-            contactMessage: {
-                vcard: `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:June-Ultra\nitem1.TEL;waid=${message.key.participant?.split('@')[0] || message.key.remoteJid.split('@')[0]}:${message.key.participant?.split('@')[0] || message.key.remoteJid.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
-            }
-        },
-        participant: "0@s.whatsapp.net"
-    };
-}
-
 module.exports = {
     name: 'ping',
     aliases: ['pong', 'p'],
@@ -30,12 +13,11 @@ module.exports = {
         try {
             const chatId = extra.from;
             const botName = config.botName || 'June-Ultra';
-            const fake = createFakeContact(msg);
 
             const start = performance.now();
             const sentMsg = await sock.sendMessage(chatId, {
                 text: applyFont('🔸 pong!...')
-            }, { quoted: fake });
+            });
 
             const ping = (performance.now() - start).toFixed(3);
             const response = applyFont(`🔹 ${botName} Speed: ${ping} ms`);
@@ -43,7 +25,7 @@ module.exports = {
             await sock.sendMessage(chatId, {
                 text: response,
                 edit: sentMsg.key
-            }, { quoted: fake });
+            });
 
         } catch (error) {
             console.error('Ping error:', error);
