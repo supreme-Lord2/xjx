81e486ecff7f2e39fe900582e4d33a58e7654578
dot-666 <dotreplit@gmail.com>
2026-06-28 14:54:29 +0300

    Update waifu.js

--- commands/anime/waifu.js
+++ commands/anime/waifu.js
@@ -1,5 +1,3 @@
-const axios = require('axios');
-
 module.exports = {
   name: 'waifu',
   aliases: ['randomwaifu'],
@@ -10,21 +8,8 @@ module.exports = {
   async execute(sock, msg, args, extra) {
     await extra.react('✨');
     try {
-      const res = await axios.get('https://api.shizo.top/sfw/waifu?apikey=shizo', { timeout: 10000 });
-      const data = res.data;
-
-      const imageUrl =
-        data?.url ||
-        data?.image ||
-        data?.image_url ||
-        null;
-
-      if (!imageUrl) {
-        return await extra.reply('❌ Could not fetch waifu image. Try again!');
-      }
-
       await sock.sendMessage(msg.key.remoteJid, {
-        image: { url: imageUrl },
+        image: { url: 'https://api.shizo.top/sfw/waifu?apikey=shizo' },
         caption: '✨ *Random Waifu*'
       }, { quoted: msg });
 
