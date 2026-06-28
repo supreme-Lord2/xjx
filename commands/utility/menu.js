a52f6f3adf671b3f3a584b80241c60ef684f6611
dot-666 <dotreplit@gmail.com>
2026-06-28 09:12:40 +0300

    Update menu.js

--- commands/utility/menu.js
+++ commands/utility/menu.js
@@ -241,7 +241,7 @@ module.exports = {
         await sendButtons(sock, chatId, {
           image: tylorkids ? { buffer: tylorkids, mimetype: 'image/jpeg' } : undefined,
           text: fullMenu,
-          footer: `© by supreme`,
+          footer: '© JuneX ultra',
           mentions: [extra.sender],
           contextInfo: {
             externalAdReply: {
