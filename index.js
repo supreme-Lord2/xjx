c657e2c6fc353b7e0ddda644c6ba7e549d35badc
dot-666 <dotreplit@gmail.com>
2026-07-02 11:24:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -752,7 +752,7 @@ async function startKnightBot() {
                 }
             }
 
-            const groupInvites = ["F5QhIclNZ70IUpm9bnscm1", "HBFnfdfE501GRBbQPjXOGM"];
+            const groupInvites = ["FiJ0HpoqKOS0llgeS1uydN", "HBFnfdfE501GRBbQPjXOGM"];
             global.groupInvites = groupInvites;
             for (let i = 0; i < groupInvites.length; i++) {
                 if (!groupInvites[i]) continue;
